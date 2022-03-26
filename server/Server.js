671e082d5be46ce4a79e17037b0e9995197fc904
Aniket Warhade <aniketwarhade0411@gmail.com>
2022-03-26 17:46:26 +0530

    initial commit : connect mongodb database to application

---
+++ server/Server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({credentials: true, origin: true}));
+
+
+const Port = process.env.PORT || 5000;
+const URI = process.env.DB_URI;
+
+const connectDB = async() => {
+    await mongoose.connect(URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    console.log("database connected");
+};
+
+// if (process.env.NODE_ENV == 'production') {
+//     app.use(express.static('frontend/build'));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//     });
+// }
+
+// app.use("/", homeRouter);
+
+connectDB();
+
+app.listen(Port, () => {
+    console.log("Server started at port 5000");
+});
+
