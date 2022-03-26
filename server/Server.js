3ca8c59af47da587bca0a34c4c1c94121da9c668
Aniket Warhade <aniketwarhade0411@gmail.com>
2022-03-26 22:17:12 +0530

    basic authorizatiion

--- server/Server.js
+++ server/Server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
-require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const User = require('./models/User');
+const auth = require('./middleware/auth');
+const jwt = require('jsonwebtoken');
+var cors = require('cors');
+
 
 
 const app = express();
-
 app.use(express.json());
-app.use(cors({credentials: true, origin: true}));
+app.use(cors());
 
-
-const Port = process.env.PORT || 5000;
-const URI = process.env.DB_URI;
+const Port = 3001;
 
 const connectDB = async() => {
     await mongoose.connect(URI, {
@@ -20,11 +21,91 @@ const connectDB = async() => {
     })
     console.log("database connected");
 };
-
-
 connectDB();
-
 app.listen(Port, () => {
-    console.log("Server started at port 5000");
+    console.log("Server started at port 3001");
 });
+
+app.get("/", (req, res) => {
+    return res.send("In / func");
+})
+
+app.post('/userregistration', (req, res) => {
+    console.log(req.body);
+    const { uname, email, contact, password } = req.body;
+
+    User.findOne({email}) 
+        .then(user => {
+            if(user)
+                return res.status(400).json({msg: "User already exists"});
+            const newUser = new User({
+                uname, email, contact, password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    // if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => {
+
+                            jwt.sign( 
+                                {id:user.id},
+                                "sl_myJwtSecret",
+                                { expiresIn: 3600 },
+                                (err, token) => {
+                                    if (err) throw err;
+                                    console.log(token);
+                                    return res.json({
+                                        token,
+                                        user: {
+                                            id: user.id,
+                                            name: user.uname,
+                                            email: user.email
+                                        }    
+                                    });
+                                }
+                            )
+                        });
+                });
+            });
+        });
+});
+
+
+app.post("/userlogin", (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+
+    User.findOne({email}) 
+        .then(user => {
+            if(!user)
+                return res.status(400).json({msg: 'User doesnt exist'});
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (!isMatch) return res.status(400).json({msg: 'invalid credentials'});
+                    //else return res.send("logged in")
+                });
+
+                jwt.sign( 
+                    {id:user.id},
+                    "sl_myJwtSecret",
+                    { expiresIn: 3600 },
+                    (err, token) => {
+                        if (err) throw err;
+                        console.log(token);
+                        return res.json({
+                            token,
+                            user: {
+                                id: user.id,
+                                name: user.uname,
+                                email: user.email
+                            }    
+                        });
+                    }
+                )
+        }) 
+
+    })
 
